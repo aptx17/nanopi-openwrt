096e379c70b6ddccfa0e39dd86e63084eb51b294
aptx17 <41042297+aptx17@users.noreply.github.com>
2020-05-04 06:43:44 +0800

    Update check_wan4.sh

--- scripts/check_wan4.sh
+++ scripts/check_wan4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 while true; do
-    wan=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+    wan=`/sbin/ifconfig |grep inet| sed -n '1p'|awk '{print $2}'|awk -F ':' '{print $2}'`
     r=`ping -c1 $wan 2>&1`
     case $r in
         *permitted* ) /etc/init.d/firewall reload ;;
     esac
-    sleep 2
+    sleep 20
 done
