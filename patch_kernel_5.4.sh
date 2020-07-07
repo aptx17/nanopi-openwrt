264e6c0a1a100039a0f9b92b875fe8d4fbc739a4
aptx17 <41042297+aptx17@users.noreply.github.com>
2020-07-07 21:06:07 +0800

    Update patch_kernel_5.4.sh

--- patch_kernel_5.4.sh
+++ patch_kernel_5.4.sh
@@ -1,6 +1,5 @@
 cd friendlywrt-rk3328
 cd kernel/
-wget -O net/netfilter/xt_FULLCONENAT.c https://raw.githubusercontent.com/Chion82/netfilter-full-cone-nat/master/xt_FULLCONENAT.c
 git apply ../../add_fullconenat.diff
 wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
 git apply RK3328-enable-1512mhz-opp.patch
