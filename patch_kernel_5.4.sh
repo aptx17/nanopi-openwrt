ab5948fcf6bc703b6609bbb0234e96eefe32a00f
aptx17 <41042297+aptx17@users.noreply.github.com>
2020-09-21 03:44:50 +0800

    Update patch_kernel_5.4.sh

--- patch_kernel_5.4.sh
+++ patch_kernel_5.4.sh
@@ -1,7 +1,7 @@
 cd friendlywrt-rk3328
 cd kernel/
 git apply ../../add_fullconenat.diff
-wget wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
+wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
 git apply RK3328-enable-1512mhz-opp.patch
 cd ../
 git clone https://github.com/openwrt/openwrt && cd openwrt/
