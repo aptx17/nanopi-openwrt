04682925122fd6cdf959e926af50c59232ba29cf
aptx17 <41042297+aptx17@users.noreply.github.com>
2020-12-02 18:49:25 +0800

    Create patch_rk3399_kernel_5.4.sh

---
+++ patch_rk3399_kernel_5.4.sh
@@ -0,0 +1,19 @@
+cd friendlywrt-rk3399
+cd kernel/
+git apply ../../add_fullconenat.diff
+cd ../
+git clone https://github.com/openwrt/openwrt && cd openwrt/
+git checkout a47279154e08d54df05fa8bf45fe935ebf0df5da
+#rm target/linux/generic/pending-5.4/403-mtd-hook-mtdsplit-to-Kbuild.patch
+#rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
+cp -a ./target/linux/generic/files/* ../kernel/
+./scripts/patch-kernel.sh ../kernel target/linux/generic/backport-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
+./scripts/patch-kernel.sh ../kernel target/linux/octeontx/patches-5.4
+cd ../
+wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
+grep -i '_NETFILTER_\|FLOW' ../.config.override > .config.override
+./merge_config.sh -m .config.override kernel/arch/arm64/configs/nanopi4_linux_defconfig && mv .config kernel/arch/arm64/configs/nanopi4_linux_defconfig
+
+cat ../scripts/kernel.seed >> kernel/arch/arm64/configs/nanopi4_linux_defconfig
